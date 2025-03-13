6ddbf5729a77ba5cb94f3a96f0d19048d83c6d66
Matthijs <stokrom@hva.nl>
2025-03-13 13:52:20 +0100

    a

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 // Gamefacation
+
+var smallNum = document.querySelector('#smallNumber');
+var bigNum = document.querySelector('#bigNumber');
+var result = document.querySelector('#result');
+
+var smallResult;
+var bigResult;
+var finalResult;
+
+function smallInput(e){
+    smallResult = e.target.value;
+    console.log(smallResult);
+}
+
+smallNum.addEventListener('input', smallInput);
+smallNum.addEventListener('input', calc);
+
+function bigInput(e){
+    bigResult = e.target.value;
+    console.log(bigResult);
+}
+
+bigNum.addEventListener('input', bigInput);
+bigNum.addEventListener('input', calc);
+
+function calc(){
+    finalResult = smallResult / bigResult;
+    finalResult = finalResult * 100;
+
+    if(smallResult && bigResult){
+        console.log(finalResult + "%");
+        result.value = finalResult;
+    }
+}
+
+// now reverse the calc
+// result.addEventListener('input', reverseCalc);
+
+// function reverseCalc(){
+
+// }
